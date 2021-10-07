096588789655bfd1065d4f07df93a9aaf93e6a4e
amagnan <a.magnan@imperial.ac.uk>
2021-10-07 10:22:37 +0200

    update iterations to allow shorter TS and later start for EM TS

--- RecoHGCal/TICL/python/TrkEMStep_cff.py
+++ RecoHGCal/TICL/python/TrkEMStep_cff.py
@@ -24,11 +24,11 @@ ticlTrackstersTrkEM = _trackstersProducer.clone(
     pid_threshold = 0.5,
     energy_em_over_total_threshold = 0.9,
     max_longitudinal_sigmaPCA = 10,
-    shower_start_max_layer = 5, #inclusive
+    shower_start_max_layer = 10, #inclusive
     max_out_in_hops = 1,
     max_missing_layers_in_trackster = 2,
     skip_layers = 2,
-    min_layers_per_trackster = 10,
+    min_layers_per_trackster = 5,
     min_cos_theta = 0.97,  # ~14 degrees
     min_cos_pointing = 0.94, # ~20 degrees
     root_doublet_max_distance_from_seed_squared = 2.5e-3, # dR=0.05
